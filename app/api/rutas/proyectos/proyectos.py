ed5b1d3f9c390dbfcf73dbe4fe1a8b3f0df692cf
JosueDnCB <162073997+JosueDnCB@users.noreply.github.com>
2025-05-27 00:11:22 -0500

    Update proyectos.py
    Se agrego el endpoint para crear proyecto

--- app/api/rutas/proyectos/proyectos.py
+++ app/api/rutas/proyectos/proyectos.py
@@ -1,9 +1,8 @@
 
 from fastapi import Path
-
+from typing import List, Dict, Any
 from fastapi import APIRouter, Query, HTTPException
 from fastapi.responses import JSONResponse
-from typing import Optional, List, Dict
 from datetime import datetime
 import pymysql
 
@@ -11,44 +10,43 @@ from app.configuracion import configuracion
 from app.servicios.servicio_simulacion import get_db_connection, simular_datos_json
 
 from app.api.modelos.simulacion import DatosSimulacion
-from app.api.modelos.simulacionJson import DatosSimulacionJson
+from app.api.modelos.proyectos import ProyectoCrear, ProyectoActualizar
 
-router = APIRouter()
-
-from pydantic import BaseModel
-from typing import List, Dict, Optional, Any
+router_proyecto = APIRouter()
 
 
-class DatoValor(BaseModel):
-    datos: Dict[str, Any] 
 
-class DatoCampo(BaseModel):
-    nombre: str              #  temperatura
-    valores: List[DatoValor] # lista de lecturas en distintos momentos
 
-class DatoSensor(BaseModel):
-    nombre: str                       # Sensor D HT22
-    campos_sensores: List[DatoCampo] # Lista de campos como temperatura, humedad...
+# Crear Proyectos 
+@router_proyecto.post("/crear_proyecto/")
+async def crear_proyecto(datos: ProyectoCrear):
+    try:
+        print(f"Nombre proyecto: {datos.nombre}")
+        print(f"Descripción: {datos.descripcion}")
+        print(f"usuario_id: {datos.usuario_id}")
 
-class DatosSimulacionJson(BaseModel):
-    proyecto: str
-    dispositivo: str
-    fecha: Optional[str] = ""
-    hora: Optional[str] = ""
-    id_paquete: int = 1
-    sensores: List[DatoSensor] #Lista de sensores relacionados al dispositivo
+   
 
-class ConfiguracionSimulacion(BaseModel):
-    url: str
-    intervalo: float
-    proyecto: str
-    dispositivo: str
+        resultados = await set_proyecto(datos)
+        
+
+        return {"message": "Se registro el proyecto", "resultados": resultados}
+
+    except ValueError as e:
+        return {"message": "Error en los datos enviados", "details": str(e)}
+    except Exception as e:
+        print("Excepción general:", type(e), e)
+        return JSONResponse(
+            status_code=500,
+            content={"message": "Error inesperado durante la inserción ", "details": str(e)},
+        )
+    
 
 
 
 # Actualizar información de proyectos
-@router.put("/ActualizarDatosProyecto/{id}")
-async def actualizar_datos_proyecto(id,datos: DatosSimulacionJson):
+@router_proyecto.put("/ActualizarDatosProyecto/{id}")
+async def actualizar_datos_proyecto(id,datos: ProyectoActualizar):
     
     try:
         print(f"Proyecto: {datos.proyecto}")
@@ -78,3 +76,60 @@ async def actualizar_datos_proyecto(id,datos: DatosSimulacionJson):
             status_code=500,
             content={"message": "Error inesperado durante la simulación", "details": str(e)},
         )
+
+
+
+#Crear proyectos
+async def set_proyecto(datos: ProyectoCrear) -> List[Dict[str, Any]]:
+    procesado = []
+    conn = None
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+       # Validar existencia del usuario
+        cursor.execute("SELECT id FROM usuarios WHERE id = %s", (datos.usuario_id))
+        usuario_row = cursor.fetchone()
+        if not usuario_row:
+            return [{
+                "status": "error",
+                "message": f"El usuario con id: '{datos.usuario_id}' no existe"
+            }]
+ 
+    
+        # Insertar el proyecto
+        cursor.execute(
+            "INSERT INTO proyectos (nombre, descripcion, usuario_id) VALUES (%s, %s, %s)",
+            (datos.nombre, datos.descripcion, datos.usuario_id)
+        )
+
+        conn.commit()
+
+        procesado.append({
+            "nombre": datos.nombre,
+            "descripcion": datos.descripcion,
+            "usuario_id": datos.usuario_id,
+            "status": "success"
+        })
+
+    except pymysql.MySQLError as e:
+        if conn:
+            conn.rollback()
+        procesado.append({
+            "status": "error",
+            "message": f"DB Error: {str(e)}"
+        })
+
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        procesado.append({
+            "status": "error",
+            "message": f"Unexpected Error: {str(e)}"
+        })
+
+    finally:
+        if conn:
+            conn.close()
+
+    return procesado
